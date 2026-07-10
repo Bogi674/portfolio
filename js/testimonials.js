4c47738c964bf13f4f218483e3389eeed59152f9
Bogiva M <148695417+Bogi674@users.noreply.github.com>
2026-07-10 19:59:45 +0700

    feat: add Work With Me section and testimonials

---
+++ js/testimonials.js
@@ -0,0 +1,25 @@
+// testimonials.js
+
+export const testimonials = [
+  {
+    quote:
+      'It was a pleasure working with Bogiva as he always remains level-headed in any situation. He is also self-driven and passionate about the works that he does, and he is not afraid to push the boundaries to take the works one step further. Overall, an awesome guy to work with!',
+    name: 'Nadya Helena',
+    role: 'Head of Marketing and Product',
+    company: 'Zuno Carbon',
+  },
+  {
+    quote:
+      'Bogi stands out especially with his knowledge and experience on digital marketing. He is friendly and able to get along with just about anyone in the team. On top of it, a reliable team player with good analytical thinking. He is also eager to learn new things and very open to discuss them. He is hands-on and willing to go the extra mile to achieve beyond project goals.',
+    name: 'Keziah',
+    role: 'Head of Business Development',
+    company: 'Astra Credit Companies (ACC)',
+  },
+  {
+    quote:
+      'Bogiva is one individual I have worked with who uniquely stands out. He displayed great works, especially for project management and product development. He managed to coordinate a project that was quite challenging and it turned out great. It is not just his technical skills that impress me, but his positive attitude toward others.',
+    name: 'Yuliana Horis',
+    role: 'Product Lead',
+    company: 'Siloam Group',
+  },
+];
